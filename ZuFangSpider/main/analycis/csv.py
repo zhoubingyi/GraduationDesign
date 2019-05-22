9b49a0b8c6cf7f649429d25fe85da93854b4ba81
12 <hyeogjoo@gmail.com>
2019-05-22 15:45:59 +0800

    大更新

---
+++ ZuFangSpider/main/analycis/csv.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+import glob
+
+csv_list = glob.glob('*.csv')  # 查看同文件夹下的csv文件数
+print(u'共发现%s个CSV文件' % len(csv_list))
+print(u'正在处理............')
+for i in csv_list:  # 循环读取同文件夹下的csv文件
+    fr = open(i, 'rb').read()
+    with open('result.csv', 'ab') as f:  # 将结果保存为result.csv
+        f.write(fr)
+print(u'合并完毕！')
