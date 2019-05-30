da2865fd6431f6e671b8664ed8232897ac030e42
momo <zhoubingyiya@gmail.com>
2019-05-30 20:45:28 +0800

    删除词云

--- ZuFangSpider/main/analycis/analycis.py
+++ ZuFangSpider/main/analycis/analycis.py
@@ -227,54 +227,7 @@ class Analycis:
                 legend_text_size=18, label_text_color=["#000"])
         bar.render()
 
-    # # 展示词云
-    def showWorkCloud(self, content, image_filename, font_filename, out_filename):
-        d = path.dirname(__name__)
-        # content = open(path.join(d, filename), 'rb').read()
-        # 基于TF-IDF算法的关键字抽取, topK返回频率最高的几项, 默认值为20, withWeight
-        # 为是否返回关键字的权重
-        tags = jieba.analyse.extract_tags(content, topK=100, withWeight=False)
-        text = " ".join(tags)
-        # 需要显示的背景图片
-        img = imread(path.join(d, image_filename))
-        # 指定中文字体, 不然会乱码的
-        wc = WordCloud(font_path=font_filename,
-                       background_color='black',
-                       # 词云形状，
-                       mask=img,
-                       # 允许最大词汇
-                       max_words=400,
-                       # 最大号字体，如果不指定则为图像高度
-                       max_font_size=100,
-                       # 画布宽度和高度，如果设置了msak则不会生效
-                       # width=600,
-                       # height=400,
-                       margin=2,
-                       # 词语水平摆放的频率，默认为0.9.即竖直摆放的频率为0.1
-                       prefer_horizontal=0.9
-                       )
-        wc.generate(text)
-        img_color = ImageColorGenerator(img)
-        plt.imshow(wc.recolor(color_func=img_color))
-        plt.axis("off")
-        plt.show()
-        wc.to_file(path.join(d, out_filename))
-
-    # 展示 pyecharts 的词云
-    def showPyechartsWordCloud(self, attr, value):
-        from pyecharts import WordCloud
-        wordcloud = WordCloud(width=1300, height=620)
-        wordcloud.add("", attr, value, word_size_range=[20, 100])
-        wordcloud.render()
-
-
-
-
 analycis = Analycis()
-
-# 构建词云
-# analycis.getTitle()
-# analycis.showWorkCloud(analycis.getTitle(), "docker.jpeg", "kh.ttf", out_filename="output.jpeg")
 
 # 统计租房面积
 # (attr, value) = analycis.getAcreage()
